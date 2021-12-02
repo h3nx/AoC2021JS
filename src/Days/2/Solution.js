359f7ca8964c119798790e1b6e4d06ea9ef83ff4
Henrik <H3nx@hotmail.se>
2021-12-02 06:27:33 +0100

    day 2 completed

--- src/Days/2/Solution.js
+++ src/Days/2/Solution.js
@@ -23,6 +23,34 @@ export function Solution2() {
     function solve(data) {
         console.log(data);
         
+        
+        let pos1 = [0,0];
+        let pos2 = [0,0];
+        let aim = 0;
+        
+        data.forEach((line) => {
+            const c = line.substring(0,line.search(/ /));
+            const n = parseInt(line.substring(line.search(/ /)+1));
+            
+            if(c === "forward") {
+                pos1[0] += n;
+                
+                pos2[0] += n;
+                pos2[1] += aim*n;
+            }
+            if(c === "down"){
+                pos1[1] += n;
+                aim+=n;
+            }
+            if(c === "up") {
+                pos1[1] -= n;
+                aim-=n;
+            }
+        });
+        
+        setAns1(pos1[0] * pos1[1]);
+        setAns2(pos2[0] * pos2[1]);
+        
     }
     
     
