28c522e49e9ca6ea1127744e59e4a9000203a086
Henrik <H3nx@hotmail.se>
2021-12-06 07:11:05 +0100

    day 6 done

--- src/Days/6/Solution.js
+++ src/Days/6/Solution.js
@@ -23,6 +23,66 @@ export function Solution6() {
     function solve(data) {
         console.log(data);
         
+        let nrs = data.map((str)=> {
+            return str.substring(15);
+        })
+        console.log(nrs);
+        let temp = nrs.map((str)=>{return str.split(",")});
+        console.log(temp);
+        let fishAll = temp.map((arr)=>{return Utils.stringToIntegerArr(arr)});
+        console.log(fishAll);
+        
+        let fish = Utils.stringToIntegerArr(data[0].split(","));
+        let fish2Input = [...fish];
+        // console.log(fish,data[0]);
+        for(let day = 0; day <= 79; day++) {
+            // console.log(day, fish);
+            if(day%10===0)
+                console.log(day, fish.length);
+            const initialLength = fish.length;
+            for(let i = 0; i < initialLength; i++) {
+                if(fish[i] === 0) {
+                    fish.push(8);
+                    fish[i] = 6;
+                } else {
+                    fish[i]--;
+                }
+            }
+        }
+        
+        setAns1(fish.length);
+        
+        
+        let fish2 = [0,0,0,0,0,0,0,0,0];
+        
+        for(let i = 0; i < fish2Input.length; i++) {
+            fish2[fish2Input[i]-1]+=1;
+        }
+        console.log("days", fish2);
+        
+        for(let day = 1; day <= 255; day++) {       
+            let prev = [...fish2];
+            let sum = 0; 
+            for(let i = 0; i < 9; i++) {
+                sum += fish2[i];
+            }
+            fish2[8] = prev[0];
+            fish2[7] = prev[8];
+            fish2[6] = prev[7]+prev[0];
+            fish2[5] = prev[6];
+            fish2[4] = prev[5];
+            fish2[3] = prev[4];
+            fish2[2] = prev[3];
+            fish2[1] = prev[2];
+            fish2[0] = prev[1];
+            // console.log(sum, fish2, prev);
+            
+        }
+        let sum = 0; 
+        for(let i = 0; i < 9; i++) {
+            sum += fish2[i];
+        }
+        setAns2(sum)
     }
     
     
